0a8f38bd4f57158af52099f4adfe2254a1360f43
Azamat <Azamat@mail.ru>
2015-04-19 23:54:33 +0300

    Finish to synchronize and create matrix.

--- Matlab/Matlab/enginedemo.cpp
+++ Matlab/Matlab/enginedemo.cpp
@@ -4,121 +4,45 @@
 #include <string.h>
 #include "engine.h"
 
-#define BUFSIZE 256
-
-static double Areal[6] = { 1, 2, 3, 4, 5, 6 };
-
-int PASCAL WinMain (HINSTANCE hInstance,
-                    HINSTANCE hPrevInstance,
-                    LPSTR     lpszCmdLine,
-                    int       nCmdShow)
-
+int main()
 {
-	Engine *ep;
-	mxArray *T = NULL, *a = NULL, *d = NULL;
-	char buffer[BUFSIZE+1];
-	double *Dreal, *Dimag;
-	double time[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int row = 2;
+	int col = 2;
 
-	/*
-	 * Start the MATLAB engine 
-	 */
-	if (!(ep = engOpen(NULL))) {
-		MessageBox ((HWND)NULL, (LPSTR)"Can't start MATLAB engine", 
-			(LPSTR) "Engwindemo.c", MB_OK);
-		exit(-1);
-	}
-
-	/*
-	 * PART I
-	 *
-	 * For the first half of this demonstration, we will send data
-	 * to MATLAB, analyze the data, and plot the result.
-	 */
-
-	/* 
-	 * Create a variable from our data
-	 */
-	T = mxCreateDoubleMatrix(1, 10, mxREAL);
-	memcpy((char *) mxGetPr(T), (char *) time, 10*sizeof(double));
-
-	/*
-	 * Place the variable T into the MATLAB workspace
-	 */
-	engPutVariable(ep, "T", T);
-
-	/*
-	 * Evaluate a function of time, distance = (1/2)g.*t.^2
-	 * (g is the acceleration due to gravity)
-	 */
-	engEvalString(ep, "D = .5.*(-9.8).*T.^2;");
-
-	/*
-	 * Plot the result
-	 */
-	engEvalString(ep, "plot(T,D);");
-	engEvalString(ep, "title('Position vs. Time for a falling object');");
-	engEvalString(ep, "xlabel('Time (seconds)');");
-	engEvalString(ep, "ylabel('Position (meters)');");
-
-	/*
-	 * PART II
-	 *
-	 * For the second half of this demonstration, we will create another mxArray
-	 * put it into MATLAB and calculate its eigen values 
-	 * 
-	 */
-	  
-	 a = mxCreateDoubleMatrix(3, 2, mxREAL);         
-	 memcpy((char *) mxGetPr(a), (char *) Areal, 6*sizeof(double));
-	 engPutVariable(ep, "A", a); 
-
-	 /*
-	 * Calculate the eigen value
-	 */
-	 engEvalString(ep, "d = eig(A*A')");
-
-	 /*
-	 * Use engOutputBuffer to capture MATLAB output. Ensure first that
-	 * the buffer is always NULL terminated.
-	 */
-	 buffer[BUFSIZE] = '\0';
-	 engOutputBuffer(ep, buffer, BUFSIZE);
-
-	 /*
-	 * the evaluate string returns the result into the
-	 * output buffer.
-	 */
-	 engEvalString(ep, "whos");
-	 MessageBox ((HWND)NULL, (LPSTR)buffer, (LPSTR) "MATLAB - whos", MB_OK);
-	
-	 /*
-	 * Get the eigen value mxArray
-	 */
-	 d = engGetVariable(ep, "d");
-	 engClose(ep);
-
-	 if (d == NULL) {
-			MessageBox ((HWND)NULL, (LPSTR)"Get Array Failed", (LPSTR)"Engwindemo.c", MB_OK);
-		}
-	else {		
-		Dreal = mxGetPr(d);
-		Dimag = mxGetPi(d);      		
-		if (Dimag)
-			sprintf(buffer,"Eigenval 2: %g+%gi",Dreal[1],Dimag[1]);
-		else
-			sprintf(buffer,"Eigenval 2: %g",Dreal[1]);
-		MessageBox ((HWND)NULL, (LPSTR)buffer, (LPSTR)"Engwindemo.c", MB_OK);
-	    mxDestroyArray(d);
-	} 
-
-	/*
-	 * We're done! Free memory, close MATLAB engine and exit.
-	 */
-	mxDestroyArray(T);
-	mxDestroyArray(a);
-	
-	return(0);
+	// Открытие MATLAB
 	Engine *Eg;
-	Eg = engOpen("\0");
+	Eg = engOpen(NULL);
+
+	// Создаем матрицу, mxArray - смешанный тип для матлаба и с++
+	mxArray *Matrix1;
+	
+	Matrix1 = mxCreateDoubleMatrix(row, col, mxREAL);
+
+	// Создаем указатель, хуй знает зачем, так мужик делал
+	double *Matrixx = mxGetPr(Matrix1);
+
+	double Arrays[] = { 1, 3, 4, 2 };
+
+	// Копируем мы тут потому из Arrays в Matrixx потому что так устроен матлаб (В видео подробно разъясняется, я нифига не понял)
+	memcpy(Matrixx, Arrays, row*col* sizeof(double));
+
+	// Создаем 2 матрицу
+	mxArray *Matrix2;
+
+	Matrix1 = mxCreateDoubleMatrix(row, col, mxREAL);
+	double *Matrixxx = mxGetPr(Matrix2);
+
+	double Arrayss[] = { 0, 5, 2, 1 };
+
+	memcpy(Matrixxx, Arrayss, row*col* sizeof(double));
+
+	engPutVariable(Eg, "Matrix2", Matrix2);
+
+	/* ТЕПЕРЬ НАДО РАЗОБРАТЬСЯ КАК ПРОИЗВОДИТЬ ВЫЧИСЛЕНИЯ С ЭТИМИ МАТРИЦАМИ, ВСЕ УЗНАЛ ИЗ ВИДЮХИ
+	*/
+
+	return 0;
 }
+	
+
+
---
+++ Matlab/Matlab/matrix_operations.h
@@ -0,0 +1,7 @@
+#include <windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "engine.h"
+
+
