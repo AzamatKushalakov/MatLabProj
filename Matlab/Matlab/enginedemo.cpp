91f4adad6318f0016379aec68f8e63a3b5a9d952
Ignatov <artoriusignatous@mail.ru>
2015-05-16 22:21:54 +0400

    no change

--- Matlab/Matlab/enginedemo.cpp
+++ Matlab/Matlab/enginedemo.cpp
@@ -13,27 +13,38 @@ int main()
 
 	// Открытие MATLAB
 	Engine *Eg;
+
 	Eg = engOpen(NULL);
 
     // создаем 1-ую матрицу
 	mxArray *Matrix1;
+
 	Matrix1 = mxCreateDoubleMatrix(row, col, mxREAL); // 1-ая матрица
+
 	double *Matrixx1 = mxGetPr(Matrix1);
+
 	double Arrays[] = { 1, 3, 4, 2 };
+
 	// Копируем мы тут потому из Arrays в Matrixx потому что так устроен матлаб 
 	memcpy(Matrixx1, Arrays, row*col* sizeof(double));
+
 	// Помещаем переменную Matrix1 в рабочую область matlab
 	engPutVariable(Eg, "Matrix1", Matrix1);
 
 	// Создаем 2 матрицу и делаем все аналогично
 	mxArray *Matrix2;
+
 	Matrix2 = mxCreateDoubleMatrix(row, col, mxREAL);
+
 	double *Matrixx2 = mxGetPr(Matrix2);
+
 	double Arrays2[] = { 0, 5, 2, 1 };
+
 	memcpy(Matrixx2, Arrays2, row*col* sizeof(double));
+
 	engPutVariable(Eg, "Matrix2", Matrix2);
 
-	// Типо складывает матрицы в рабочей области matlab
+	// складываем матрицы в рабочей области matlab
 	engEvalString(Eg, "M_sum = Matrix + Matrix2");
 
 	return 0;
